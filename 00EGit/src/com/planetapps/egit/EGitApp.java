84e67fdd8ea14d14a4abbdca8491a407216d40af
casper <casper@DESKTOP-KAQ0Q6P.fios-router.home>
2018-06-08 11:43:47 -0400

    First EGit commit

---
+++ 00EGit/src/com/planetapps/egit/EGitApp.java
@@ -0,0 +1,10 @@
+package com.planetapps.egit;
+
+public class EGitApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, EGit!!");
+	}
+
+}
