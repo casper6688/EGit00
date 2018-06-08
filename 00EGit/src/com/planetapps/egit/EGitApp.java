6e090071727a37f4a867c5c6747b2d8c212bd211
casper <casper@home>
2018-06-08 15:59:25 -0400

    First commit

--- 00EGit/src/com/planetapps/egit/EGitApp.java
+++ 00EGit/src/com/planetapps/egit/EGitApp.java
@@ -5,6 +5,7 @@ public class EGitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, EGit!!");
+		System.out.println("Second line.");
 	}
 
 }
